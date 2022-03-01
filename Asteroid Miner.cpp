5004055896c8a3d22cb1cfded36a90ebd4a08cb7
Tymianox <tymlegiec@gmail.com>
2022-03-01 14:38:29 +0100

    cleanup

--- Asteroid Miner.cpp
+++ Asteroid Miner.cpp
@@ -1,14 +1,13 @@
 /*
-	"Asteroid Miner"
-	Autor: Tymoteusz Legiêæ
-	Na podstawie https://www.youtube.com/watch?v=rMKHLz3liuk i https://www.youtube.com/watch?v=8Z1VqLd6U34
+	Project: Asteroid Miner
+	Autor: Tymoteusz "Tymianox"
+	Description: Asteroid Miner is a simple game where you have to catch all asteroids coming your way. How many can you catch? Can you save Earth from running out of resources?
 */
 
 #include <iostream> //cout, cin
 #include <conio.h> //getch
-#include <cstdlib> //system
+//#include <cstdlib> //system
 #include <time.h> //srand
-//#include <graphics.h>
 using namespace std;
 
 //definicje zmiennych
@@ -16,21 +15,21 @@ int y=20, x=12, i=0, j=0, wspXstatku=5, wspXasteroid=(rand()%(x-2)+1), wspYaster
 char plansza[20][12];
 
 /*
-	y - wysokoœæ planszy
-	x - szerokoœæ planszy
-	i, j - zmienne do pêtli
-	wspXstatku - wspó³rzêdna x statku
-	wspXasteroid - wspó³rzêdna x asteroid
-	wspYasteroid - wspó³rzêdna y asteroid
-	klawisz - naciœniêty klawisz
+	y - wysokoï¿½ï¿½ planszy
+	x - szerokoï¿½ï¿½ planszy
+	i, j - zmienne do pï¿½tli
+	wspXstatku - wspï¿½rzï¿½dna x statku
+	wspXasteroid - wspï¿½rzï¿½dna x asteroid
+	wspYasteroid - wspï¿½rzï¿½dna y asteroid
+	klawisz - naciï¿½niï¿½ty klawisz
 */
 
 main() {
 	
-	//konfiguracja generatora liczb za pomoc¹ czasu
+	//konfiguracja generatora liczb za pomocï¿½ czasu
 	srand(time(NULL));
   
-  //wstêpna konfiguracja planszy
+  //wstï¿½pna konfiguracja planszy
   while (i<y) {
     j=0;
     while (j<x) {
@@ -74,23 +73,23 @@ main() {
     i++;
 	}
   
-  //wstrzymanie a¿ do naciœniêcia dowolnego klawisza
+  //wstrzymanie aï¿½ do naciï¿½niï¿½cia dowolnego klawisza
   klawisz=getch();
   
   /////////////////////////////
   
-  //g³ówna pêtla gry
+  //gï¿½ï¿½wna pï¿½tla gry
   while (wspYasteroid<y) {
   	
   	//czyszczenie ekranu
   	system("cls");
     
-    //jeœli naciœniêto klawisz
+    //jeï¿½li naciï¿½niï¿½to klawisz
     if (kbhit()) {
-    	klawisz=getch(); //weŸ wartoœæ klawisza
-    	if (klawisz==224)klawisz+=getch(); //poniewa¿ wartoœæ klawisza mo¿e byæ wiêksza ni¿ 224 getch podaje drug¹ liczbê któr¹ trzeba dodaæ do pierwszej
+    	klawisz=getch(); //weï¿½ wartoï¿½ï¿½ klawisza
+    	if (klawisz==224)klawisz+=getch(); //poniewaï¿½ wartoï¿½ï¿½ klawisza moï¿½e byï¿½ wiï¿½ksza niï¿½ 224 getch podaje drugï¿½ liczbï¿½ ktï¿½rï¿½ trzeba dodaï¿½ do pierwszej
     
-    	//ruch statku w prawo/lewo w zale¿noœci od wciœniêtego klawisza (strza³ki i wasd)
+    	//ruch statku w prawo/lewo w zaleï¿½noï¿½ci od wciï¿½niï¿½tego klawisza (strzaï¿½ki i wasd)
 			if (klawisz==100||klawisz==301) {
     		wspXstatku=wspXstatku+1;
   		} else if (klawisz==97||klawisz==299) {
@@ -98,18 +97,18 @@ main() {
 			}
 		}
     
-    //zwiêkszanie wyniku przy "z³apaniu" asteroidy oraz generowanie nowej
+    //zwiï¿½kszanie wyniku przy "zï¿½apaniu" asteroidy oraz generowanie nowej
     if (wspYasteroid>=y-3&&wspXasteroid==wspXstatku) {
 			wynik++;
 			wspXasteroid=(rand()%(x-2)+1);
 			wspYasteroid=0;
 		}
 		
-		//blokada ruchu statku poza planszê
+		//blokada ruchu statku poza planszï¿½
     if (wspXstatku==x-1) { wspXstatku=x-2; }
     if (wspXstatku==0) { wspXstatku=1; }
     
-    //przypisywanie odnoœników do ró¿nych czêœci statku i asteroidy
+    //przypisywanie odnoï¿½nikï¿½w do rï¿½nych czï¿½ci statku i asteroidy
     plansza[y-3][wspXstatku]='g', plansza[y-2][wspXstatku-1]='t', plansza[y-2][wspXstatku]='y', plansza[y-2][wspXstatku+1]='u', plansza[y-1][wspXstatku]='b';
     plansza[wspYasteroid][wspXasteroid]='a', plansza[wspYasteroid+1][wspXasteroid]='a';
     
@@ -132,22 +131,22 @@ main() {
         while (j<x) {
         	switch (plansza[i][j]) {
         		case 'a':
-							cout << "OO"; //czêœæ asteroidy
+							cout << "OO"; //czï¿½ï¿½ asteroidy
 							break;
             case 'p':
 							cout << "  "; //puste pole
 							break;
 						case 'g':
-							cout << "db"; //dziób statku
+							cout << "db"; //dziï¿½b statku
 							break;
 						case 't':
-							cout << " x"; //lewe skrzyd³o statku
+							cout << " x"; //lewe skrzydï¿½o statku
 							break;
 						case 'y':
-							cout << "XX"; //kad³ub statku
+							cout << "XX"; //kadï¿½ub statku
 							break;
 						case 'u':
-							cout << "x "; //prawe skrzyd³o statku
+							cout << "x "; //prawe skrzydï¿½o statku
 							break;
 						case 'b':
 							cout << "^^"; //silniki statku
@@ -164,11 +163,11 @@ main() {
     	i++;
 		}
     
-    //czyszczenie pól po statku i asteroidzie
+    //czyszczenie pï¿½l po statku i asteroidzie
     plansza[y-3][wspXstatku]='p', plansza[y-2][wspXstatku-1]='p', plansza[y-2][wspXstatku]='p', plansza[y-2][wspXstatku+1]='p', plansza[y-1][wspXstatku]='p';
     plansza[wspYasteroid][wspXasteroid]='p';
     
-    //ruch asteroidy w dó³
+    //ruch asteroidy w dï¿½
     wspYasteroid++;
   }
   
